d3956240bb3b66a808e915d4921d90f16d1139a3
T-jay Abunales <tjabunales@gmail.com>
2024-10-06 15:32:31 +0800

    user model

---
+++ src/models/userModel.mjs
@@ -0,0 +1,45 @@
+import fs from 'fs-extra';
+import bcrypt from 'bcryptjs';
+import { v4 as uuidv4 } from 'uuid';
+const path = './data/users.json';
+
+const ROUNDS = 10;
+
+export async function getUsers() {
+    try {
+        return await fs.readJson(path);
+    } catch (err) {
+        return [];
+    }
+}
+
+export async function saveUsers(users) {
+    await fs.writeJson(path, users);
+}
+
+export async function createUser(user) {
+    const users = await getUsers();
+    const salt = await bcrypt.genSalt(ROUNDS);
+    user.password = await bcrypt.hash(user.password, salt);
+    user.id = uuidv4();
+    users.push(user);
+    user.role = user.role || 'user'
+    await saveUsers(users);
+}
+
+export async function findUserByEmail(email) {
+    const users = await getUsers();
+    return users.find(user => user.email === email);
+}
+
+export async function findUserById(id) {
+    const users = await getUsers();
+    return users.find(user => user.id === id);
+}
+
+export async function findUserByUsername(username) {
+    const users = await getUsers();
+    return users.find(user => user.username === username);
+}
+
+export default { getUsers, createUser, findUserByEmail, findUserById, findUserByUsername };
